e26bc173af489425db82b66eccce04122e4d4835
smilina702 <32021201+smilina702@users.noreply.github.com>
2017-11-13 19:30:44 +0300

    Update hwork3.py

--- hwork3/hwork3.py
+++ hwork3/hwork3.py
@@ -1,3 +1,3 @@
-word = str(input('введите слово: '))
+word = str(input('введите слово: ')) #Можно использовать и str для получения нужного результата
 for i in range(1, len(word)+1):
     print(word[:i])
