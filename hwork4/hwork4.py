2af333aadfbeed12b18c590052ec3151dbb137ce
smilina702 <32021201+smilina702@users.noreply.github.com>
2017-11-20 21:44:53 +0300

    Update hwork4.py

--- hwork4/hwork4.py
+++ hwork4/hwork4.py
@@ -1,8 +1,8 @@
 print("Вариант 2")
 with open("text1.txt", encoding = "utf -8-sig") as f:
     lines = f.readlines()
-if len(lines) <=1: 
-    print("недостаточно строк")
+if len(lines) <= 1: 
+    print("Недостаточно строк")
 else:
     min_line = len(lines[0].replace("\n", "")) #убираем символ переноса в минимальной и максимальной строке
     max_line = len(lines[0].replace("\n", "")) 
@@ -12,5 +12,5 @@ else:
             max_line = len(line)
         if len(line) <= min_line and len(line) != 0: #минимальная строка должна содержать хотя бы один символ
             min_line = len(line)
-    print('Самая короткая строка короче самой длинной в', round(max_line / min_line, 5), 'раз(а)')
+    print('Самая короткая строка короче самой длинной в', round(max_line / min_line, 2), 'раз(а)')
     
