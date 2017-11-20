c3494827f0a3d2df4a843ae84a9761f970ed2fd1
smilina702 <32021201+smilina702@users.noreply.github.com>
2017-11-20 21:32:28 +0300

    Update hwork4.py

--- hwork4/hwork4.py
+++ hwork4/hwork4.py
@@ -1,13 +1,16 @@
 print("Вариант 2")
 with open("text1.txt", encoding = "utf -8") as f:
     lines = f.readlines()
-min_line = len(lines[0].replace("\n", "")) #убираем символ переноса в минимальной и максимальной строке
-max_line = len(lines[0].replace("\n", "")) 
-for line in lines:
-    line = line.replace("\n", "") #убираем символ переноса 
-    if len(line) >= max_line:
-        max_line = len(line)
-    if len(line) <= min_line and len(line) != 0: #минимальная строка должна содержать хотя бы один символ
-        min_line = len(line)
-print('Самая короткая строка короче самой длинной в', round(max_line / min_line, 5), 'раз(а)')
+if len(lines) <=1: 
+    print("недостаточно строк")
+else:
+    min_line = len(lines[0].replace("\n", "")) #убираем символ переноса в минимальной и максимальной строке
+    max_line = len(lines[0].replace("\n", "")) 
+    for line in lines:
+        line = line.replace("\n", "") #убираем символ переноса 
+        if len(line) >= max_line:
+            max_line = len(line)
+        if len(line) <= min_line and len(line) != 0: #минимальная строка должна содержать хотя бы один символ
+            min_line = len(line)
+     print('Самая короткая строка короче самой длинной в', round(max_line / min_line, 5), 'раз(а)')
     
