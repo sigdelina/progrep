78bdaa2cf94115c75246282a253f28eb0c693d35
smilina702 <32021201+smilina702@users.noreply.github.com>
2017-12-06 20:48:56 +0300

    Update hwork5.py

--- hwork5/hwork5.py
+++ hwork5/hwork5.py
@@ -1,12 +1,15 @@
 print ("Вариант 2")
 word = input("Add word: ")
+word_list = []
+word_list.append(word)
+while word != "":
+    word = input("Add word: ")
+    word_list.append(word)
 with open("text1.txt", "w", encoding = "utf-8-sig") as f:
-    word = input("Add new word: ")
-    while word != "":
+    for word in word_list:
         if len(word) > 5:
             f.write("\n")
             f.write(word)
-        word = input("Add new word: ")
 with open("text1.txt", encoding = "utf-8") as final_file:
     print(final_file.read().strip())
     
