ddaa259a072e49fa82fe9992074b9b48f65fe293
smilina702 <32021201+smilina702@users.noreply.github.com>
2017-12-12 13:19:23 +0300

    Update kontra.py

--- control/kontra.py
+++ control/kontra.py
@@ -1,4 +1,5 @@
 print("2 вариант")
+
 print("задание 1")
 with open("Ozhegov.txt", "r", encoding = "utf-8")as file:
     for line in file:
@@ -6,3 +7,16 @@ with open("Ozhegov.txt", "r", encoding = "utf-8")as file:
         l = word[0].split(" ")
         if len(l[0]) >= 20:
             print(line)
+
+print("задание 2")
+a = []
+with open("Ozhegov.txt", "r", encoding = "utf-8")as file:
+    for line in file:
+        word = line.split("|")
+        #line = line.replace("123456789\n ", "")
+        if word[2] != "":
+            a.append(line)
+            #print(line) или print(word[2]) если нужно вывести строки с данными словами или сами слова
+print(len(a))
+            
+print("задание 3")
