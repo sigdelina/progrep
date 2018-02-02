3456c9cd316683733876b200fb80d2c12727e54c
smilina702 <32021201+smilina702@users.noreply.github.com>
2018-02-02 23:02:56 +0300

    Add files via upload

---
+++ bonus/1.py
@@ -0,0 +1,18 @@
+a = input("Введите число: ")
+number = 0
+summa = 0
+a_list = []
+a_list.append(a)
+if a == '':
+    print("Нет числа")
+else:
+    while a != '':
+        a = input("Введите число: ")
+        a_list.append(a)
+    del a_list[-1]
+    a_list = list(map(int, a_list))
+    print(a_list)
+    for a in a_list:
+        summa += a
+        number += 1
+    print("Минимальное число: ", min(a_list), ".", "Наибольшее число: " , max(a_list), ".", "Среднее арифмитическое: ", summa/number)
