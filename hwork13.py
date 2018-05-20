7ed44117c8c58bf9d858983f42a2f2df68f9c2fa
smilina702 <32021201+smilina702@users.noreply.github.com>
2018-05-20 18:56:21 +0300

    Add files via upload

---
+++ hwork13.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+# До конца не было понятно, что требудется.
+# Могут ли быть в названии папки неалфавитные символы (подчеркивания и т.д.)?
+# "сколько в дереве папок с полностью кириллическими названиями"
+# - поэтому программа нахожит папки c названиями ТОЛЬКО из кириллических символов, в название программы включается пробел. 
+
+def find_dirs():
+    dirs_list = []
+    start_path = '.'
+    for root, dirs, files in os.walk(start_path):
+        for dir_names in dirs:
+            print(dir_names)
+            dirs_names = re.findall('^[А-Яа-яЁё\s]+$', dir_names) 
+            if len(set(dirs_names)) == len(dirs_names):
+                print(dirs_names)
+                dirs_list.append(dirs_names)
+    return dirs_list
+
+def dir_list(dirs_list):
+    count = []
+    for dirs in dirs_list:
+        if dirs != []:
+            count.append(dirs)
+    return len(count)
+
+def main():
+    general = find_dirs()
+    counting = dir_list(general)
+    print("В дереве папок с полностью киррилическими названиями", counting, "штуки.")
+    
+main()
