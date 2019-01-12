f9dcaf861547f9c384689b31d5df95b57ce91ff9
smilina702 <32021201+smilina702@users.noreply.github.com>
2019-01-12 19:50:15 +0300

    Update heeelp me.py

--- nis_project/heeelp me.py
+++ nis_project/heeelp me.py
@@ -6,78 +6,34 @@ with open('sentences.csv', 'w', encoding='utf-8') as file:
     row = '%s\t%s\t%s\t%s\t%s\t%s\t%s\n'
     file.write(row % ('Path', 'Tag', 'Full sentence', 'Tagged mistake', 
                    'Corrected mistake', 'Correct/incorrect', 'Group'))
-path = './data/exam2014' # прописывает путь
+path = './data/' # прописывает путь
 # обходит дерево
 for root, dirs, files in os.walk(path):
     for file in files:
         if ".ann" in file: # для файлов с нужным расширением
             filename = str(file)
-            print(root, dirs, file)
+         #   print(root, dirs, file)
             final_ann = os.path.join(root, file) # полный путь к файлу
             with open(final_ann, encoding="utf-8") as file:
                 annot_txt = file.read()
           #  print(annot_txt)
             clean_text = annot_txt.splitlines() # список строк
-            for line in clean_text: # обход строк в списке
+            for i in range(len(clean_text)-1): # обход строк в списке
               #  print(line)
-                find_tag = re.search(r'T.*?Tense_choice.*?\D', line) # для строки с нужным тегом
+                find_tag = re.search(r'T.*?Tense_choice.*?', clean_text[i]) # для строки с нужным тегом
                 if find_tag is not None:
-                    tag_mistake = re.search(r'T\d.*\d\s(.*)', line, flags=re.DOTALL) # ошибка
+                    tag_id = re.search(r'(T\d*?)\s', line)
+                    identif = tag_id.group(1)
+                    tag_id_path = os.path.join(final_ann[7:-4], identif)
+                    print(tag_id_path)
+                    tag_mistake = re.search(r'T\d.*\d\s(.*)', clean_text[i], flags=re.DOTALL) # ошибка
                     mistake = tag_mistake.group(1)
-                    position = clean_text.index(line)
-                   # print(tag_mistake)
-                   # print(position)
-                    pos_for_wrong = position + 1 # позиция с ошибкой + 1
-                   # print(pos_for_wrong)
-                    сorrection = clean_text[pos_for_wrong]
-                    #print(сorrection)
-                   # tgg = re.search(r'^T\d.*\d\s(.*)', correction, flags=re.DOTALL)
-                    tag_correct = re.search(r'#.*T\d.*\s(.*)', сorrection, flags=re.DOTALL)
-#                     #if tag_correct is None:
-#                     #    pos_for_wrong += 1
-                    
-
-                    
-# #                     if tgg is None:
-# #                         pos_for_wrong += 1 # позиция строки с ошибкой
-# #                         сorrection = clean_text[pos_for_wrong]
-# #                         tgg = re.search(r'^T\d.*\d\s(.*)', correction, flags=re.DOTALL)
-#                     while tgg is None:
-#                         pos_for_wrong += 1 # позиция строки с ошибкой
-#                     pos = pos_for_wrong -1
-#                     correction = clean_text[pos]
-#                     tag_correct = re.search(r'#.*T\d.*\s(.*)',  сorrection, flags=re.DOTALL)
-#                     if tag_correct is None:
-#                         correct = ' - '
-#                     else:
-#                         correct = tag_correct.group(1)
-#                     print(correct)
-                  #  tag_place = re.search(r'\s(\d*?)\s(\d*?)\s', line, flags=re.DOTALL)
-                  #  tag_start = tag_place.group(1)
-                  #  tag_end = tag_place.group(2)
-                   # print(tag_start, '\n', tag_end)
-                  #  text_file = filename[0:-3] + 'txt'
-                  #  final_text = os.path.join(root, text_file)
-                  #  with open(final_text, encoding="utf-8") as ftxt:
-                  #      clear_text = ftxt.read()
-                  #  clean = clear_text.replace('\n','')
-                    
-                    #вот здесь нужно это предложение вытащить из файла и чет я запуталась
-                    
-                  #  text_string = list(clean.replace(' ', ''))
-                  #  print(clean)
-                  #  mistake_in_txt = len(clean)
-                  #  clean_list = clean.split('.')
-                  #  for sentence in clean_list:
-                  #      if clean.find(sentence) <= int(tag_start) and clean.find(sentence) + len(sentence) >= int(tag_end):
-                  #          print(sentence)
-                    #print(clean_list)
-                  #  print(mistake_in_txt)
+                    tag_correct = re.search(r'#.*T\d.*\s(.*)', clean_text[i+1], flags=re.DOTALL)
+                    if tag_correct is None:
+                        correct = ''
+                    else:
+                        correct = tag_correct.group(1)
                 # пополенение таблицы
                     with open('sentences.csv', 'a', encoding='utf-8') as f:
                         row = '%s\tTense_choice\t%s\t%s\t%s\t%s\t%s\n'
-                        f.write(row % (final_ann, "th", mistake, correct, 'h', 't'))
-                        
-                    
-                    
-            
+                        f.write(row % (tag_id_path, "th", mistake, correct, 'h', 't'))
